fe9c82580b08b7557f287899af28fe40efed5575
Piotr Przechlewski <haesor@gmail.com>
2018-01-29 20:49:46 +0100

    add script.js and index.html

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = concat(femaleNames, maleNames);
+var newName = "Marian"
+if (allNames.indexOf(newName) === -1);
+{
+maleNames.push ("newName")
+}
